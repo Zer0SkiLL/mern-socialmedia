7d1f6735c5f69dfbce2481a94054ab011d52d2d4
Zer0SkiLL <retowidmer95@hotmail.com>
2023-06-23 01:12:36 +0200

    login and registerpage frontend

--- frontend/src/scenes/loginPage/index.jsx
+++ frontend/src/scenes/loginPage/index.jsx
@@ -2,32 +2,36 @@ import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
 import Form from "./Form";
 
 const LoginPage = () => {
-    const theme = useTheme();
-    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
-    return (
-        <Box>
-            <Box width="100%" backgroundColor={theme.palette.background.alt} p="1rem 6%" textAlign="center">
-                <Typography 
-                    fontWeight="bold" 
-                    fontSize="32px" 
-                    color="primary" >
-                    MernPedia
-                </Typography>
-            </Box>  
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          MernPedia
+        </Typography>
+      </Box>
 
-            <Box 
-                width={isNonMobileScreens ? "50%" : "93%"}
-                p="2rem"
-                m="2rem auto"
-                borderRadius="1.5rem"
-                backgroundcolor={theme.palette.background.alt} 
-            >
-                <Typography fontWeight="500" variant="h5" sx={{mb: "1.5"}}>
-                    Welcome to MernPedia for everyone that got banned from 5chan
-                </Typography>
-            </Box>          
-        </Box>
-    )
-}
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundcolor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to MernPedia for everyone that got banned from 5chan
+        </Typography>
+
+        <Form />
+      </Box>
+    </Box>
+  );
+};
 
 export default LoginPage;
